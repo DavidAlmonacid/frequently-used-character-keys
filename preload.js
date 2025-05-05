d2079dc98377d1fdb7327c297384597a56868977
David Almonacid <david.almonacidf@gmail.com>
2025-05-05 18:25:41 -0500

    Remove commented-out clipboard code and clean up preload.js

--- preload.js
+++ preload.js
@@ -1,14 +1,6 @@
-// const { clipboard } = require("electron");
 const { contextBridge, ipcRenderer } = require("electron/renderer");
 
 contextBridge.exposeInMainWorld("electronAPI", {
   copyToClipboard: (text) => ipcRenderer.invoke("clipboard-copy", text),
   pasteFromClipboard: () => ipcRenderer.invoke("clipboard-paste"),
 });
-
-// const { contextBridge, ipcRenderer } = require('electron');
-
-// contextBridge.exposeInMainWorld('electronAPI', {
-//   copyToClipboard: (text) => ipcRenderer.invoke('clipboard:copy', text),
-//   pasteFromClipboard: () => ipcRenderer.invoke('clipboard:paste')
-// });
