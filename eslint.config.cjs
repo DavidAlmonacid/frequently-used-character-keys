ffc2b404539d05e152193e76fd011367cf6a48d9
David Almonacid <david.almonacidf@gmail.com>
2025-05-05 19:07:10 -0500

    refactor: consolidate ESLint configuration into eslint.config.cjs and remove eslint.config.js

---
+++ eslint.config.cjs
@@ -0,0 +1,18 @@
+const js = require("@eslint/js");
+const eslintPluginPrettierRecommended = require("eslint-plugin-prettier/recommended");
+const { defineConfig, globalIgnores } = require("eslint/config");
+const globals = require("globals");
+
+module.exports = defineConfig([
+  globalIgnores(["package-lock.json"]),
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+  },
+  eslintPluginPrettierRecommended,
+]);
