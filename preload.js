c774b1d7e8373ae7dcaef1a56f1b165489906f1f
David Almonacid <david.almonacidf@gmail.com>
2025-05-05 19:06:41 -0500

    fix: remove unused pasteFromClipboard function from electronAPI

--- preload.js
+++ preload.js
@@ -2,5 +2,4 @@ const { contextBridge, ipcRenderer } = require("electron/renderer");
 
 contextBridge.exposeInMainWorld("electronAPI", {
   copyToClipboard: (text) => ipcRenderer.invoke("clipboard-copy", text),
-  pasteFromClipboard: () => ipcRenderer.invoke("clipboard-paste"),
 });
