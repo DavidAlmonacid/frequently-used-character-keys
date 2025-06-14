4b863f8cda151631aae63f53fcf29263ac8ac3b0
David Almonacid <david.almonacidf@gmail.com>
2025-06-14 17:00:01 -0500

    feat: implement lazy loading for database connection to avoid top level await

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,17 +1,28 @@
 import Database, { type QueryResult } from "@tauri-apps/plugin-sql";
 
-const db = await Database.load("sqlite:characters.db");
+let dbPromise: Promise<Database> | null = null;
+
+function getDb(): Promise<Database> {
+  if (dbPromise === null) {
+    dbPromise = Database.load("sqlite:characters.db");
+  }
+
+  return dbPromise;
+}
 
 export async function getAllCharacters(): Promise<Array<Character>> {
+  const db = await getDb();
   return await db.select("SELECT id, character FROM characters;");
 }
 
 export async function addCharacter(character: string): Promise<QueryResult> {
+  const db = await getDb();
   return await db.execute("INSERT INTO characters (character) VALUES ($1);", [
     character
   ]);
 }
 
 export async function deleteCharacter(id: number) {
+  const db = await getDb();
   return await db.execute("DELETE FROM characters WHERE id = $1;", [id]);
 }
