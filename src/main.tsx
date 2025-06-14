cbe655e155d43d7519c9f5fc501def7ed6bab528
David Almonacid <david.almonacidf@gmail.com>
2025-06-14 15:15:56 -0500

    Organize imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
-import "@fontsource-variable/space-grotesk";
 import { render } from "preact";
 
 import App from "./App";
 import "./main.css";
+
+import "@fontsource-variable/space-grotesk";
 
 render(<App />, document.getElementById("root")!);
